507189bf589f335c49cf60303eb07926a64532df
annguyen <annguyen.it.utc@gmail.com>
2022-11-27 11:30:55 +0700

    initial commit

---
+++ src/main/java/com/example/ems/ExaminationManagementBeApplication.java
@@ -0,0 +1,12 @@
+package com.example.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminationManagementBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminationManagementBeApplication.class, args);
+	}
+}
