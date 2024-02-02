9803ec481b3130728cb11420a8cdc031713c9ec4
An Nguyen <annguyen.it.utc@gmail.com>
2024-02-02 23:30:26 +0700

    fix(dep): bump to version 6.0.18

--- ESM.Infrastructure/Migrations/20240202143333_BumpDotNetToVersion6.0.18.Designer.cs
+++ ESM.Infrastructure/Migrations/20240202143333_BumpDotNetToVersion6.0.18.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ESM.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ESM.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240202143333_BumpDotNetToVersion6.0.18")]
+    partial class BumpDotNetToVersion6018
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ESM.Infrastructure/Migrations/20240202143333_BumpDotNetToVersion6.0.18.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ESM.Infrastructure.Migrations
+{
+    public partial class BumpDotNetToVersion6018 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
